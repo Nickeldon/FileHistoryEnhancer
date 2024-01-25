70e3c586e7636f51096c6d362572290fa6c3d8ce
Nickeldon <50726216+Nickeldon@users.noreply.github.com>
2024-01-25 12:38:32 -0500

    added features to UI (Still not usable)

--- UI/handler.js
+++ UI/handler.js
@@ -1,9 +1,11 @@
 localStorage.clear()
+document.getElementById('sec-notif').style.transform = 'scale(0)'
 
 function colormode(){
     if(document.getElementById('menu1').style.backgroundColor === 'white'){
         document.getElementById('filter').style.opacity = '70%'
         document.getElementById('menu1').style.backgroundColor = 'black'
+        document.getElementById('sec-notif').style.backgroundColor = 'black'
         document.getElementById('warn-multichoice').style.backgroundColor = 'black'
         document.getElementById('warn-recursive').style.backgroundColor = 'black'
         document.getElementById('menu1').style.color = 'white'
@@ -15,9 +17,14 @@ function colormode(){
             document.getElementsByClassName('choice-rec')[i].style.backgroundColor = 'black'
         }
         document.getElementsByClassName('gg-dark-mode')[0].style.backgroundColor = 'black'
+        document.getElementById('sec-notif').style.color = 'white'
+        document.querySelector('.security-notification input').style.color = 'white'
+        document.querySelector('.security-notification input').style.backgroundColor = 'black'
+        document.querySelector('.security-notification input').style.borderColor = 'white'
     } else{
         document.getElementById('filter').style.opacity = '5%'
         document.getElementById('menu1').style.backgroundColor = 'white'
+        document.getElementById('sec-notif').style.backgroundColor = 'white'
         document.getElementById('warn-multichoice').style.backgroundColor = 'white'
         document.getElementById('warn-recursive').style.backgroundColor = 'white'
         document.getElementById('menu1').style.color = 'black'
@@ -29,23 +36,74 @@ function colormode(){
             document.getElementsByClassName('choice-rec')[i].style.backgroundColor = 'white'
         }
         document.getElementsByClassName('gg-dark-mode')[0].style.backgroundColor = null
+        document.getElementById('sec-notif').style.color = 'black'
     }
 }
 
-function handler(){
-    document.getElementsByClassName('btn-select')[0].children[0].innerHTML = 'Folder selected'
-    var files = []
-    var metadata
-    files.push(document.getElementById('div-select').files)
-    document.getElementById('path-ph').innerHTML = 'C://example/FileHistory'
-    files.forEach(element => {
-        console.log(element)
-    });
-    console.log(document.getElementById('div-select'))
+function handler(value){
+    if(!value){
+    
+    document.getElementById('sec-notif').style.visibility = 'visible'
+    document.getElementById('sec-notif').style.transform = 'scale(1)'
+    document.getElementsByClassName('btn-select')[0].children[0].innerHTML = 'Folder selected'} 
+    else{
+        var path = document.getElementById('full-path').value
+        document.getElementById('path-ph').innerHTML = path
+        document.getElementById('sec-notif').style.transform = 'scale(0)'
+
+        var metadata = {"data": path}
+
+        fetch(`http://localhost:8000/request?METADATA=${JSON.stringify(metadata)}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(response => {
+    if(response.status === 404){
+      console.log('path was not valid')
+    }
+    console.log(response)})
+    .then(message => {
+      console.log(message);
+      // Handle the response as needed
+    })
+    .catch(error => console.error('Error:', error));
+
+        /*fetch(url, {
+            method: 'GET',
+          })
+          .then(response => {console.log(response)})
+          .then(data => {
+            // Handle the transformed data received from the server
+            console.log('Transformed Data:', data);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });*/
+
+        /*const xhttp = new XMLHttpRequest()
+        xhttp.open("GET", tempurl, false)
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send()*/
+        /*fetch(`http://localhost:5501/request?METADATA=${metadata}`,{
+                method: "GET",
+                headers: {"Content-Type": "application/json"}
+            }).then(res => {
+                res.json()
+            console.log(res)})
+            .then(mes => {
+                console.log(mes)
+            })
+            .catch(err => {
+                console.log('ERROR: ', err)
+            })*/
+    }
 }
 
 function recursivech(choice){
     if(choice){
+      
         
     }else{
 
--- server.js
+++ server.js
@@ -1,4 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const PORT = 8000;
 const fs = require('fs')
-const path = require('path')
-const http = require('htttp')
+const app = express();
+app.use(cors())
+app.listen(PORT, () => {
+    console.log(`Server listening on Port ${PORT}`);
+});
 
+app.get('/request', (req, res, next) => {
+    var path = JSON.parse(req.query.METADATA).data
+    console.log('Received data:', path);
+    if(VerifyPath(path)){
+        res.send('valid path')
+    }else{
+        res.sendStatus(404)
+    }
+});
+
+function VerifyPath(path){
+    try {
+        if(fs.existsSync(path)){
+            return true
+        }else{
+            return false
+        }
+    } catch (e) {
+        return false
+    }
+}
